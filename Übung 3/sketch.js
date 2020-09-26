c0d7bc520c83b4518e982eea15a949d55b82737a
Joshua Kehrer <joshua.kehrer@bluewin.ch>
2020-09-26 11:20:08 +0200

    change colors and structure

--- Übung 3/sketch.js
+++ Übung 3/sketch.js
@@ -3,25 +3,26 @@ function setup() {
 }
 
 function draw() {
-  let c1 = color('#625261')
-  let c2 = color('#a6a6a4')
+  let c1 = color('#4c4c4c')
+  let c2 = color('#a37eba')
   let c3 = color('#e8e8e8')
-  let c4 = color('#89beb3')
+  let c4 = color('#e5df88')
+  let c5 = color('#f56a79')
   background(c2);
   stroke(50);
   strokeWeight(0.8);
   //fill(c2);
   
-  for (let x =0; x <= width; x = x + 50) {  
-    for (let y =0; y <= height; y = y + 100) {
+  for (let x =0; x <= width; x = x + 55) {  
+    for (let y =0; y <= height; y = y + 150) {
       fill(c1);
-      rect(x,y,30,200);
+      rect(x,y+x/50,30,100);
       fill(c3);
-      rect(x,y,25,50);
+      rect(x,y,20,90);
       fill(c4);
-      ellipse(x,y,70,70);
-      fill(c2);
-      ellipse(x,y,50,50);
+      ellipse(x,y-x/20,70-x/20,70-x/20);
+      fill(c5);
+      ellipse(x,y+x/20,20+x/20,20+x/20);
     }
   }
 
